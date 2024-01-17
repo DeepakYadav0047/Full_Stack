3989e34f71dcb1139dd80767583950f5ffb9053d
gyanly <gyancv@gmail.com>
2024-01-17 11:40:38 +0530

    first commit

---
+++ src/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    const navbar = "Blog";
+    const url = "http://google.com"
+    return(
+        <>
+        <h2>{navbar}</h2>
+        <a href={url}>Google</a>
+        </>
+    )
+}
+
+export default Navbar
